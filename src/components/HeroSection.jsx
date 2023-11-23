4c832e494587cee32c5cd3587eacc97906bf029f
Mohd Hassan <mohdh7408@gmail.com>
2023-11-23 10:14:02 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -28,6 +28,34 @@ const HeroSection = () => {
                 <MdOutlineSearch className="text-white" size={24} />
               </span>
             </div>
+
+            <div className="mt-4 flex items-center gap-3">
+              <div className="avatar-group -space-x-6 rtl:space-x-reverse">
+                <div className="avatar">
+                  <div className="w-12">
+                    <img src="https://images.unsplash.com/flagged/photo-1573740144655-bbb6e88fb18a?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                  </div>
+                </div>
+                <div className="avatar">
+                  <div className="w-12">
+                    <img src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                  </div>
+                </div>
+                <div className="avatar">
+                  <div className="w-12">
+                    <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                  </div>
+                </div>
+                <div className="avatar placeholder">
+                  <div className="w-12 bg-neutral text-neutral-content">
+                    <span>+99</span>
+                  </div>
+                </div>
+              </div>
+              <p className="hover:text-purple-400 cursor-pointer font-semibold ">
+                Join over 500,000 satisfied learners! Explore our courses now.
+              </p>
+            </div>
           </div>
         </div>
       </div>
